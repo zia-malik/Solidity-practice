075a55d4ef982ff3745aa46d60631c89833b8e6c
Remix Workspace <>
2022-09-05 16:11:56 +0500

    initial

--- contracts/NFT Marketplace/NFTStaking.sol
+++ contracts/NFT Marketplace/NFTStaking.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.11;
+//NFTStaking
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
